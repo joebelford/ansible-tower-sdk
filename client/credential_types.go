d1d3307e4d571aa3e9cad3999947fbbed69355ee
Montassar <bouagina.montassar@gmail.com>
2021-11-23 20:50:54 +0100

    Add credential Type CRUD

--- client/credential_types.go
+++ client/credential_types.go
@@ -1,5 +1,11 @@
 package tower
 
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+)
+
 type CredentialTypeService struct {
 	client *Client
 }
@@ -25,4 +31,77 @@ func (cs *CredentialTypeService) ListCredentialsTypes(params map[string]string) 
 	}
 
 	return result.Results, result, nil
+}
+func (cs *CredentialTypeService) CreateCredentialsTypes(data map[string]interface{}, params map[string]string) (*CredentialType, error) {
+    result := new(CredentialType)
+    payload, err := json.Marshal(data)
+    if err != nil {
+        return nil, err
+    }
+
+    resp, err := cs.client.Requester.PostJSON(credentialTypesAPIEndpoint, bytes.NewReader(payload), result, params)
+    if err != nil {
+        return nil, err
+    }
+
+    err = CheckResponse(resp)
+    if err != nil {
+        return nil, err
+    }
+
+    return result, nil
+}
+
+func (cs *CredentialTypeService) GetCredentialsTypesByID(id int, params map[string]string) (*CredentialType, error) {
+    result := new(CredentialType)
+    endpoint := fmt.Sprintf("%s%d", credentialTypesAPIEndpoint, id)
+    resp, err := cs.client.Requester.GetJSON(endpoint, result, params)
+    if err != nil {
+        return nil, err
+    }
+
+    err = CheckResponse(resp)
+    if err != nil {
+        return nil, err
+    }
+
+    return result, nil
+}
+
+func (cs *CredentialTypeService) UpdateCredentialsTypesByID(id int, data map[string]interface{},
+    params map[string]string) (*CredentialType, error) {
+    result := new(CredentialType)
+    endpoint := fmt.Sprintf("%s%d", credentialTypesAPIEndpoint, id)
+
+    payload, err := json.Marshal(data)
+    if err != nil {
+        return nil, err
+    }
+
+    resp, err := cs.client.Requester.PatchJSON(endpoint, bytes.NewReader(payload), result, params)
+    if err != nil {
+        return nil, err
+    }
+
+    err = CheckResponse(resp)
+    if err != nil {
+        return nil, err
+    }
+
+    return result, nil
+}
+
+func (cs *CredentialTypeService) DeleteCredentialsTypesByID(id int, params map[string]string) error {
+    endpoint := fmt.Sprintf("%s%d", credentialTypesAPIEndpoint, id)
+    resp, err := cs.client.Requester.Delete(endpoint, nil, params)
+    if err != nil {
+        return err
+    }
+
+    err = CheckResponse(resp)
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
